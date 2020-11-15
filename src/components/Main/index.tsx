4ad32de6c6a4136fd822c238618dea056b0c0fee
Fernando Silva <fernandosilva@hotmail.com>
2020-11-15 18:00:38 -0300

    Creating style for the first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import * as S from './styles';
 
 function Main() {
   return (
-    <main>
-      <h1>React avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um atomo e React Avançado escrito ao lado"
+      />
+      <S.Title>React avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, Nextjs e styled components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Ilustração de um desenvolvedor de frente para uma tela com códigos"
+      />
+    </S.Wrapper>
   );
 }
 
